564fe0cea35954207fe87d7a6d9a3695ff7386db
sabiss <sabrinabezsilva@gmail.com>
2023-12-22 13:25:22 -0300

    FEAT: rotas de usuários

---
+++ src/routes/usuarioRoutes.ts
@@ -0,0 +1,8 @@
+import { criarController } from "../modules/usuarios";
+import { Router, Request, Response } from "express";
+
+const routes = Router();
+
+routes.post("/usuario", (req: Request, res: Response) => {
+  criarController.handle(req, res);
+});
