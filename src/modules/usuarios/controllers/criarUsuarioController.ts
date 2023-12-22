2577053cd1702621a0c1df381fa4249d8da2c0da
sabiss <sabrinabezsilva@gmail.com>
2023-12-22 20:49:05 -0300

    REFACTOR: tipando dados vindo do corpo da requisição

--- src/modules/usuarios/controllers/criarUsuarioController.ts
+++ src/modules/usuarios/controllers/criarUsuarioController.ts
@@ -1,10 +1,12 @@
+import { UsuarioDTO } from "../dtos/usuarioDTO";
 import { CriarUsuarioServices } from "../services/criarUsuarioService"; //só tipa
 import { Request, Response } from "express";
+import { UsuarioMappingPrisma } from "../mapping/usuariosMappingPrisma";
 
 export class CriarUsuarioController {
   constructor(private readonly service: CriarUsuarioServices) {}
   async handle(req: Request, res: Response): Promise<Response> {
-    const usuario = req.body;
+    const usuario = <UsuarioDTO>req.body;
     await this.service.execute(usuario);
     return res.status(200).send({ message: "Usuário criado" });
   }
