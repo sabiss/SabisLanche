418e2a274d66a86c0bdfb9bc134d5848c17f4d98
sabiss <sabrinabezsilva@gmail.com>
2023-12-22 20:48:32 -0300

    REFACTOR: alterando conversao

--- src/modules/usuarios/services/criarUsuarioService.ts
+++ src/modules/usuarios/services/criarUsuarioService.ts
@@ -1,9 +1,11 @@
 import { UsuarioRepository } from "../repositories/usuarioRepository"; //só serve pra tipar, o repositório que será usado mesmo é passado no parâmetro no arquivo index.ts
-import { UsuarioEntity } from "../entities/usuarioEntity";
+import { UsuarioMapping } from "../mapping/usuariosMapping";
+import { UsuarioDTO } from "../dtos/usuarioDTO";
 
 export class CriarUsuarioServices {
   constructor(private readonly usuarioRepository: UsuarioRepository) {}
-  async execute(user: UsuarioEntity): Promise<void> {
-    const res = await this.usuarioRepository.criar(user);
+  async execute(user: UsuarioDTO): Promise<void> {
+    const usuario = UsuarioMapping.dtoParaEntity(user);
+    await this.usuarioRepository.criar(usuario);
   }
 }
