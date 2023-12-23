5cd84b7eaeb560d6f532c111cd621bf49867a1ea
sabiss <sabrinabezsilva@gmail.com>
2023-12-23 08:31:24 -0300

    REFACTOR: adicionando status

--- src/modules/pedidos/controllers/listarPedidosController.ts
+++ src/modules/pedidos/controllers/listarPedidosController.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
 import { ListarPedidosService } from "../services/listarPedidosService";
-import { PedidoDTO } from "../dtos/pedidosDTO";
 
 export class ListarPedidosController {
   constructor(private readonly service: ListarPedidosService) {}
   async handle(req: Request, res: Response) {
     const pedidos = await this.service.execute();
-    return pedidos;
+    return res.status(200).json(pedidos);
   }
 }
