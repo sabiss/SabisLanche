4c6305bdb15ce8a08623014b7cc4d96baa9254d6
sabiss <sabrinabezsilva@gmail.com>
2023-12-23 14:41:50 -0300

    FIX: adicionando tratamento de erros

--- src/modules/pedidos/controllers/listarUmPedidoController.ts
+++ src/modules/pedidos/controllers/listarUmPedidoController.ts
@@ -5,7 +5,14 @@ export class ListarUmPedidoController {
   constructor(private readonly service: ListarUmPedidoService) {}
   async handle(req: Request, res: Response): Promise<Response> {
     const id = req.params.id;
-    const pedido = await this.service.execute(Number(id));
-    return res.status(200).json(pedido);
+    try {
+      const pedido = await this.service.execute(Number(id));
+      if (!pedido) {
+        return res.status(404).send({ message: "Pedido não encontrado" });
+      }
+      return res.status(200).json(pedido);
+    } catch (error) {
+      return res.status(500).send({ message: "Erro ao buscar Pedido" });
+    }
   }
 }
