83058e5377e6ab4a798fd7f49536d0f2f3cab5d1
sabiss <sabrinabezsilva@gmail.com>
2023-12-23 13:46:19 -0300

    FEAT: usuario deve conter a busca por id

--- src/modules/usuarios/repositories/usuarioRepository.ts
+++ src/modules/usuarios/repositories/usuarioRepository.ts
@@ -2,4 +2,5 @@ import { UsuarioEntity } from "../entities/usuarioEntity";
 
 export interface UsuarioRepository {
   criar(usuario: UsuarioEntity): Promise<void>;
+  buscarPorId(id: number): Promise<UsuarioEntity>;
 }
