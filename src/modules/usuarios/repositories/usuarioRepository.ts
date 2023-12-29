dcdb9fb2ded75aea08dd90672353fa541187a8a0
sabiss <sabrinabezsilva@gmail.com>
2023-12-29 20:47:20 -0300

    FIX: criar recebe um DTO

--- src/modules/usuarios/repositories/usuarioRepository.ts
+++ src/modules/usuarios/repositories/usuarioRepository.ts
@@ -1,6 +1,7 @@
+import { UsuarioDTO } from "../dtos/usuarioDTO";
 import { UsuarioEntity } from "../entities/usuarioEntity";
 
 export interface UsuarioRepository {
-  criar(usuario: UsuarioEntity): Promise<void>;
+  criar(usuario: UsuarioDTO): Promise<void>;
   buscarPorId(id: number): Promise<UsuarioEntity>;
 }
