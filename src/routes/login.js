e1e81f0d3179b45a261b2360aa0bb95b2a446fa8
sabiss <sabrinabezsilva@gmail.com>
2023-12-29 19:29:46 -0300

    FIX: deletando arquivo js desnecessário

--- src/routes/login.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import authController from "../controllers/authController.js";
-
-const routes = new Router();
-routes.post("/login", authController.logar);
-
-export default routes;
