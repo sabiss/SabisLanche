cff1cd09c71447713a77762666d3b6359154a3a7
sabiss <sabrinabezsilva@gmail.com>
2023-12-29 19:45:05 -0300

    REFACTOR: comentando arquivo não utilizado, mas importante

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,23 +1,22 @@
-import usuarioModel from "../models/usuarioModel.js";
 import jwt from "jsonwebtoken";
 
-const auth = async (req, res, next) => {
-  if (!req.headers.authorization) {
-    return res.status(401).json({ message: "Não autorizado" });
-  }
-  const token = req.headers.authorization.split(" ")[1];
-  try {
-    const payload = jwt.verify(token, process.env.SECRET); //verifica o token e retorna o payload
+// const auth = async (req, res, next) => {
+//   if (!req.headers.authorization) {
+//     return res.status(401).json({ message: "Não autorizado" });
+//   }
+//   const token = req.headers.authorization.split(" ")[1];
+//   try {
+//     const payload = jwt.verify(token, process.env.SECRET); //verifica o token e retorna o payload
 
-    const usuario = await usuarioModel.buscarPorId(payload.id);
-    if (!usuario) {
-      return res.status(401).json({ message: "Não autorizado" });
-    }
-  } catch (error) {
-    return res.status(401).json({ message: "Não autorizado" });
-  }
+//     const usuario = await
+//     if (!usuario) {
+//       return res.status(401).json({ message: "Usuário não cadastrado" });
+//     }
+//   } catch (error) {
+//     return res.status(401).json({ message: "Não autorizado" });
+//   }
 
-  next();
-};
+//   next();
+// };
 
-export default auth;
+// export default auth;
