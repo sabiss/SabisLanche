713b12ba0486f94e89fdf064b4db8ad53785f7bf
sabiss <sabrinabezsilva@gmail.com>
2023-12-29 19:26:18 -0300

    FEAT: adicionando handler errors

--- app.ts
+++ app.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-import routes from "./src/routes";
-import "express-async-errors";
+import routes from "./src/routes/index";
+import { errorHandler } from "./src/middleware/error.handle";
 
 const app = express();
 
-app.use(cors({ origin: "*" }), express.json(), routes);
+app.use(cors({ origin: "*" }), express.json(), routes, errorHandler);
 
 app.listen(3000, () => {
   console.log("A API está rodando na porta 3000");
