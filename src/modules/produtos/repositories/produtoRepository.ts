2119c2b830d1f2a004cbc0e0dd3e23e54d05cc8c
sabiss <sabrinabezsilva@gmail.com>
2023-12-30 07:49:23 -0300

    FEAT: interface de repositório para produtos

--- src/modules/produtos/repositories/produtoRepository.ts
+++ src/modules/produtos/repositories/produtoRepository.ts
@@ -2,5 +2,5 @@ import { ProdutoEntity } from "../entities/produtoEntity";
 
 export interface ProdutoRepository {
   listarProdutos(): Promise<ProdutoEntity[]>;
-  listarUmProduto(): Promise<ProdutoEntity>;
+  listarUmProduto(id: number): Promise<ProdutoEntity>;
 }
