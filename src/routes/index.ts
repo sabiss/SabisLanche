b40110d11ab1917af4539ac475f8f3380de1834d
sabiss <sabrinabezsilva@gmail.com>
2023-12-30 08:12:05 -0300

    FEAT: importando rotas dos produtos

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,10 @@ import { Router } from "express";
 import usuarios from "./usuarioRoutes";
 import pedidos from "./pedidosRoutes";
 import login from "./loginRoutes";
+import produtos from "./produtosRoutes";
 
 const routes = Router();
 
-routes.use(usuarios, pedidos, login);
+routes.use(usuarios, pedidos, login, produtos);
 
 export default routes;
