66ecb8b18f98fb79b929e9df9e3eb2b88996f1ef
sabiss <sabrinabezsilva@gmail.com>
2023-12-30 10:26:51 -0300

    REFACTOR: tratando erros e acertos

--- src/modules/pedidos/controllers/atualizarPedidoController.ts
+++ src/modules/pedidos/controllers/atualizarPedidoController.ts
@@ -4,9 +4,13 @@ import { Request, Response } from "express";
 export class AtualizarPedidoController {
   constructor(private readonly service: AtualizarPedidoService) {}
   async handle(req: Request, res: Response): Promise<Response> {
-    const id = req.params.id;
-    const { novoIdProduto, novaObservacao } = req.body;
-    await this.service.execute(Number(id), novoIdProduto, novaObservacao);
-    return res.status(200).send({ message: "Pedido Atualizado" });
+    try {
+      const id = req.params.id;
+      const { novoIdProduto, novaObservacao } = req.body;
+      await this.service.execute(Number(id), novoIdProduto, novaObservacao);
+      return res.status(200).send({ message: "Pedido Atualizado" });
+    } catch (error) {
+      return res.status(500).send({ message: "Erro ao atualizar Pedido" });
+    }
   }
 }
